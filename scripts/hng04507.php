df5e3ce11fbd8b4e08dddf31e9ed00e877d9f077
Israelkingz <iodeajo@gmail.com>
2020-06-02 19:37:22 +0100

    update_task

--- scripts/hng04507.php
+++ scripts/hng04507.php
@@ -1,8 +1,6 @@
-
 <?php 
 $fullname = 'Odeajo Israel';
 $id = 'HNG-04507';
 $lang = 'php';
-
 echo "Hello World, this is " . $fullname . " with HNG7 ID  ".  $id . " using " . $lang . " for stage2 task";
 ?>
