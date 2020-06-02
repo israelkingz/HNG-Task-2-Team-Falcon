e4bbd31f8f5f05b1d6a4c4fcc13ee48830a46927
Israelkingz <iodeajo@gmail.com>
2020-06-02 19:30:58 +0100

    update_hng7

---
+++ scripts/hng04507.php
@@ -0,0 +1,8 @@
+
+<?php 
+$fullname = 'Odeajo Israel';
+$id = 'HNG-04507';
+$lang = 'php';
+
+echo "Hello World, this is " . $fullname . " with HNG7 ID  ".  $id . " using " . $lang . " for stage2 task";
+?>
