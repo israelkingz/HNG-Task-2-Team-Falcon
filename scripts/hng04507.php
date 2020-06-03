6cca79e82bd586e4d6ca4d196b671bf50086663a
Israelkingz <iodeajo@gmail.com>
2020-06-03 09:41:31 +0100

    submission_update

--- scripts/hng04507.php
+++ scripts/hng04507.php
@@ -2,5 +2,6 @@
 $fullname = 'Odeajo Israel';
 $id = 'HNG-04507';
 $lang = 'php';
+$email ='iodeajo@gmail.com';
 echo "Hello World, this is " . $fullname . " with HNG7 ID  ".  $id . " using " . $lang . " for stage2 task";
 ?>
